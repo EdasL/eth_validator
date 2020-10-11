8218a564af09db70677e7d22dd35379d33f43ea2
juliakas <julius.rupsys98@gmail.com>
2020-10-11 18:41:38 +0300

    First implementation of get_shard_transition_fields func

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub mod validator {
-    use std::ptr::eq;
+    use std::{collections::HashSet, ptr::eq};
 
     pub struct BeaconState {
         pub shard_states: Vec<ShardState>,
@@ -60,12 +60,6 @@ pub mod validator {
     }
 
     fn process_shard_block(shard_state: &mut ShardState, block: &ShardBlock) {
-        // shard_state.slot = block.slot
-        // prev_gasprice = shard_state.gasprice
-        // shard_block_length = len(block.body)
-        // shard_state.gasprice = compute_updated_gasprice(prev_gasprice, uint64(shard_block_length))
-        // if shard_block_length != 0:
-        //     shard_state.latest_block_root = hash_tree_root(block)
         shard_state.slot = block.slot;
         let prev_gasprice = shard_state.gasprice;
         let shard_block_length = block.body.len();
@@ -78,11 +72,13 @@ pub mod validator {
     fn compute_updated_gasprice(prev_gasprice: Gwei, shard_block_length: u64) -> Gwei {
         if shard_block_length > TARGET_SHARD_BLOCK_SIZE {
             let delta = prev_gasprice * (shard_block_length - TARGET_SHARD_BLOCK_SIZE)
-                / TARGET_SHARD_BLOCK_SIZE / GASPRICE_ADJUSTMENT_COEFFICIENT;
+                / TARGET_SHARD_BLOCK_SIZE
+                / GASPRICE_ADJUSTMENT_COEFFICIENT;
             std::cmp::min(prev_gasprice + delta, MAX_GASPRICE)
         } else {
             let delta = prev_gasprice * (TARGET_SHARD_BLOCK_SIZE - shard_block_length)
-            / TARGET_SHARD_BLOCK_SIZE / GASPRICE_ADJUSTMENT_COEFFICIENT;
+                / TARGET_SHARD_BLOCK_SIZE
+                / GASPRICE_ADJUSTMENT_COEFFICIENT;
             std::cmp::max(prev_gasprice, MIN_GASPRICE + delta) - delta
         }
     }
@@ -138,6 +134,139 @@ pub mod validator {
         (shard_block_lengths, shard_data_roots, shard_states)
     }
 
+    pub fn get_shard_winning_roots(
+        state: &BeaconState,
+        attestations: &Vec<Attestation>,
+    ) -> (Vec<Shard>, Vec<Root>) {
+        let mut shards = Vec::<Shard>::new();
+        let mut winning_roots = Vec::<Root>::new();
+        let online_indices = get_online_validator_indices(state);
+        let on_time_attestation_slot = compute_previous_slot(state.slot);
+        let committee_count =
+            get_committee_count_per_slot(state, compute_epoch_at_slot(on_time_attestation_slot));
+        for committee_index in 0..committee_count {
+            let shard = compute_shard_from_committee_index(
+                state,
+                committee_index,
+                on_time_attestation_slot,
+            );
+            let mut shard_attestations = Vec::<&Attestation>::new();
+            for attestation in attestations {
+                if is_on_time_attestation(state, &attestation.data)
+                    && attestation.data.index == committee_index
+                {
+                    shard_attestations.push(attestation);
+                }
+            }
+            let committee = get_beacon_committee(state, on_time_attestation_slot, committee_index);
+            let shard_transition_roots = shard_attestations
+                .iter()
+                .map(|x| x.data.shard_transition_root)
+                .collect::<HashSet<Root>>();
+            let sorted_shard_trans_roots = {
+                let mut shard_transition_roots =
+                    shard_transition_roots.into_iter().collect::<Vec<Root>>();
+                shard_transition_roots.sort_unstable();
+                shard_transition_roots
+            };
+
+            for shard_transition_root in sorted_shard_trans_roots {
+                //             transition_attestations = [
+                //                 a for a in shard_attestations
+                //                 if a.data.shard_transition_root == shard_transition_root
+                //             ]
+                let transition_attestations = shard_attestations
+                    .iter()
+                    .filter(|a| a.data.shard_transition_root == shard_transition_root)
+                    .map(|a| *a)
+                    .collect::<Vec<&Attestation>>();
+
+                let mut transition_participants = HashSet::<ValidatorIndex>::new();
+                for attestation in transition_attestations {
+                    let participants = get_attesting_indices(
+                        state,
+                        &attestation.data,
+                        attestation.aggregation_bits,
+                    );
+                    transition_participants = transition_participants
+                        .union(&participants.into_iter().collect())
+                        .map(|x| *x)
+                        .collect();
+                    let enough_online_stake = {
+                        let online_indices = online_indices
+                            .iter()
+                            .map(|x| *x)
+                            .collect::<HashSet<ValidatorIndex>>();
+                        let committee = committee
+                            .iter()
+                            .map(|x| *x)
+                            .collect::<HashSet<ValidatorIndex>>();
+                        get_total_balance(
+                            state,
+                            &online_indices
+                                .intersection(&transition_participants)
+                                .map(|x| *x)
+                                .collect(),
+                        ) * 3
+                            >= get_total_balance(
+                                state,
+                                &online_indices
+                                    .intersection(&committee)
+                                    .map(|x| *x)
+                                    .collect(),
+                            ) * 2
+                    };
+
+                    if enough_online_stake {
+                        shards.push(shard);
+                        winning_roots.push(shard_transition_root);
+                        break;
+                    }
+                }
+            }
+        }
+
+        (shards, winning_roots)
+    }
+    // def get_shard_winning_roots(state: BeaconState,
+    //                             attestations: Sequence[Attestation]) -> Tuple[Sequence[Shard], Sequence[Root]]:
+    //     shards = []
+    //     winning_roots = []
+    //     online_indices = get_online_validator_indices(state)
+    //     on_time_attestation_slot = compute_previous_slot(state.slot)
+    //     committee_count = get_committee_count_per_slot(state, compute_epoch_at_slot(on_time_attestation_slot))
+    //     for committee_index in map(CommitteeIndex, range(committee_count)):
+    //         shard = compute_shard_from_committee_index(state, committee_index, on_time_attestation_slot)
+    //         # All attestations in the block for this committee/shard and are "on time"
+    //         shard_attestations = [
+    //             attestation for attestation in attestations
+    //             if is_on_time_attestation(state, attestation.data) and attestation.data.index == committee_index
+    //         ]
+    //         committee = get_beacon_committee(state, on_time_attestation_slot, committee_index)
+
+    //         # Loop over all shard transition roots, looking for a winning root
+    //         shard_transition_roots = set(a.data.shard_transition_root for a in shard_attestations)  # non-duplicate
+    //         for shard_transition_root in sorted(shard_transition_roots):
+    //             transition_attestations = [
+    //                 a for a in shard_attestations
+    //                 if a.data.shard_transition_root == shard_transition_root
+    //             ]
+    //             transition_participants: Set[ValidatorIndex] = set()
+    //             for attestation in transition_attestations:
+    //                 participants = get_attesting_indices(state, attestation.data, attestation.aggregation_bits)
+    //                 transition_participants = transition_participants.union(participants)
+
+    //             enough_online_stake = (
+    //                 get_total_balance(state, online_indices.intersection(transition_participants)) * 3 >=
+    //                 get_total_balance(state, online_indices.intersection(committee)) * 2
+    //             )
+    //             if enough_online_stake:
+    //                 shards.append(shard)
+    //                 winning_roots.append(shard_transition_root)
+    //                 break
+
+    //     return shards, winning_roots
+
     // Mocks for get_shard_winning_roots
 
     // arg types
@@ -153,6 +282,7 @@ pub mod validator {
         pub beacon_block_root: Root,
         pub source: Checkpoint,
         pub target: Checkpoint,
+        pub shard_transition_root: Root,
     }
 
     pub struct Attestation {
@@ -163,7 +293,7 @@ pub mod validator {
 
     // Functions
 
-    pub fn get_online_validator_indices(_state: BeaconState) -> Vec<ValidatorIndex> {
+    pub fn get_online_validator_indices(_state: &BeaconState) -> Vec<ValidatorIndex> {
         let mut set_of_validator_indexes = Vec::<ValidatorIndex>::new();
 
         // push anything depending on your needs while implementing
@@ -182,52 +312,64 @@ pub mod validator {
     }
 
     // Return the number of committees in each slot for the given ``epoch``.
-    pub fn get_committee_count_per_slot(_state: BeaconState, _epoch: Epoch) -> u64 {
+    pub fn get_committee_count_per_slot(_state: &BeaconState, _epoch: Epoch) -> u64 {
         10 as u64
     }
 
-    pub fn compute_shard_from_committee_index(_state: BeaconState, index: CommitteeIndex, _slot: Slot) -> Shard {
+    pub fn compute_shard_from_committee_index(
+        _state: &BeaconState,
+        index: CommitteeIndex,
+        _slot: Slot,
+    ) -> Shard {
         // 1 represents shard start value at slot, modify if needed
         (index + 1) % INITIAL_ACTIVE_SHARDS
     }
 
     // Check if the given ``attestation_data`` is on-time.
-    pub fn is_on_time_attestation(state: BeaconState, attestation_data: AttestationData) -> bool {
-      return eq(&attestation_data.slot, &compute_previous_slot(state.slot));
+    pub fn is_on_time_attestation(state: &BeaconState, attestation_data: &AttestationData) -> bool {
+        return eq(&attestation_data.slot, &compute_previous_slot(state.slot));
     }
 
     // Return the epoch number at ``slot``
     pub fn compute_epoch_at_slot(slot: Slot) -> Epoch {
-       return slot / SLOTS_PER_EPOCH;
+        return slot / SLOTS_PER_EPOCH;
     }
 
     // Return the beacon committee at ``slot`` for ``index``
-    pub fn get_beacon_committee(_state: BeaconState, _slot: Slot, _index: CommitteeIndex) -> Vec<ValidatorIndex> {
+    pub fn get_beacon_committee(
+        _state: &BeaconState,
+        _slot: Slot,
+        _index: CommitteeIndex,
+    ) -> Vec<ValidatorIndex> {
         let mut set_of_validator_indexes = Vec::<ValidatorIndex>::new();
 
-         // push anything depending on your needs while implementing
-         set_of_validator_indexes.push(1 as u64);
-         set_of_validator_indexes.push(2 as u64);
- 
-         set_of_validator_indexes
+        // push anything depending on your needs while implementing
+        set_of_validator_indexes.push(1 as u64);
+        set_of_validator_indexes.push(2 as u64);
+
+        set_of_validator_indexes
     }
 
     // Return the set of attesting indices corresponding to ``data`` and ``bits``.
-    pub fn get_attesting_indices(state: BeaconState, data: AttestationData, _bits: BitList) -> Vec<ValidatorIndex> {
+    pub fn get_attesting_indices(
+        state: &BeaconState,
+        data: &AttestationData,
+        _bits: BitList,
+    ) -> Vec<ValidatorIndex> {
         let mut set_of_validator_indexes = Vec::<ValidatorIndex>::new();
         let _committee: std::vec::Vec<u64> = get_beacon_committee(state, data.slot, data.index);
 
         // push anything depending on your needs while implementing
         set_of_validator_indexes.push(1 as u64);
         set_of_validator_indexes.push(2 as u64);
- 
+
         set_of_validator_indexes
     }
 
     // Return the combined effective balance of the ``indices``.
     // ``EFFECTIVE_BALANCE_INCREMENT`` Gwei minimum to avoid divisions by zero.
     // Math safe up to ~10B ETH, afterwhich this overflows uint64.
-    pub fn get_total_balance(_state: BeaconState, _indices: Vec<ValidatorIndex>) -> Gwei {
+    pub fn get_total_balance(_state: &BeaconState, _indices: &Vec<ValidatorIndex>) -> Gwei {
         return EFFECTIVE_BALANCE_INCREMENT;
     }
 }
